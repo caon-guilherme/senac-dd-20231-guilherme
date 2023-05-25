a68d9185c1eb2eb6656de79e24240e60a0aae62d
guilherme.caon <guilherme.caon@FLE207D12.alunos.sc.senac.br>
2023-05-24 21:55:26 -0300

    AE!

---
+++ src/main/java/paineis/Tela10.java
@@ -0,0 +1,66 @@
+package paineis;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import java.awt.Color;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Tela10 extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Tela10 frame = new Tela10();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Tela10() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBackground(new Color(255, 255, 0));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JMenuBar menuBar = new JMenuBar();
+		menuBar.setBounds(0, 0, 101, 22);
+		contentPane.add(menuBar);
+		
+		JMenu mnRelatorio = new JMenu("Relatórios");
+		menuBar.add(mnRelatorio);
+		
+		JMenuItem mntmRelatorioVerde = new JMenuItem("Relatório Verde");
+		mnRelatorio.add(mntmRelatorioVerde);
+		
+		JMenuItem mntmRelatorioAzul = new JMenuItem("Relatório Azul");
+		mntmRelatorioAzul.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			
+			}
+		});
+		mnRelatorio.add(mntmRelatorioAzul);
+	}
+}
